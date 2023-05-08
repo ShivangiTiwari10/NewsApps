05407032d9373d9b819f32dcda7a60f8985af786
Shivangi <shivangit1111@gmail.com>
2023-05-08 22:42:01 +0530

    feat: get news detail

--- app/src/main/java/com/example/newsapp/NewsDetailActivity.kt
+++ app/src/main/java/com/example/newsapp/NewsDetailActivity.kt
@@ -2,7 +2,9 @@ package com.example.newsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+
 import com.example.newsapp.databinding.ActivityNewsDetailBinding
+import com.squareup.picasso.Picasso
 
 class NewsDetailActivity : AppCompatActivity() {
     private lateinit var binding: ActivityNewsDetailBinding
@@ -12,5 +14,22 @@ class NewsDetailActivity : AppCompatActivity() {
         binding = ActivityNewsDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
         supportActionBar?.hide()
+
+        val heading = intent.getStringExtra("heading")
+        val detail = intent.getStringExtra("content")
+        val image = intent.getStringExtra("Image")
+        val content = intent.getStringExtra("detail")
+
+        val images = binding.imageView
+        val headingTV = binding.textView2
+        val contentText = binding.textView3
+        val description = binding.Detail
+
+        headingTV.text = heading
+        contentText.text = content
+        description.text = detail
+        Picasso.get().load(image).into(images)
+
+
     }
 }
