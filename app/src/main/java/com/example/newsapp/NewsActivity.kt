f42e2f0d7641476ee68f4a2a20253c5a17e60219
Shivangi <shivangit1111@gmail.com>
2023-05-08 13:34:39 +0530

    feat: bind adapter class

--- app/src/main/java/com/example/newsapp/NewsActivity.kt
+++ app/src/main/java/com/example/newsapp/NewsActivity.kt
@@ -2,10 +2,20 @@ package com.example.newsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.newsapp.adapter.NewsAdapter
 import com.example.newsapp.databinding.ActivityNewsBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class NewsActivity : AppCompatActivity() {
     private lateinit var binding: ActivityNewsBinding
+
+    private lateinit var myadapter: NewsAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -13,5 +23,33 @@ class NewsActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         supportActionBar?.hide()
+
+        val retrofitBuilder = Retrofit.Builder()
+
+            .baseUrl("https://newsapi.org/v2/")
+            .addConverterFactory((GsonConverterFactory.create()))
+            .build()
+            .create(NewsApiInterface::class.java)
+
+        val retrofitData = retrofitBuilder.getNewsData()
+
+        retrofitData.enqueue(object : Callback<newsData?> {
+            override fun onResponse(call: Call<newsData?>, response: Response<newsData?>) {
+
+                val responceBody = response.body()
+                val articleList = responceBody?.articles
+                myadapter = NewsAdapter(this@NewsActivity, articleList!!)
+                binding.myRecycler.adapter = myadapter
+
+                binding.myRecycler.layoutManager = LinearLayoutManager(this@NewsActivity)
+
+            }
+
+            override fun onFailure(call: Call<newsData?>, t: Throwable) {
+
+                Log.d("MyRetrofitActivity", "onFailure ${t.message}")
+            }
+        })
+
     }
 }
