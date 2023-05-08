67b176728d2536ce263e1e5b1a652e40b68af4aa
Shivangi <shivangit1111@gmail.com>
2023-05-08 09:40:11 +0530

    feat: create splashScreen

--- app/src/main/java/com/example/newsapp/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -3,13 +3,32 @@ package com.example.newsapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.newsapp.databinding.ActivityMainBinding
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val activityScope = CoroutineScope(Dispatchers.Main)
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        supportActionBar?.hide()
+
+        activityScope.launch {
+            delay(2000)
+
+
+            finish()
+        }
     }
+
+    override fun onPause() {
+        activityScope.cancel()
+        super.onPause()
+    }
+
 }
