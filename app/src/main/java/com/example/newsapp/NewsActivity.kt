bd1fe886f46419fe4c3ec5e162727f547d928aa7
Shivangi <shivangit1111@gmail.com>
2023-05-08 09:49:02 +0530

    feat: implement binding

--- app/src/main/java/com/example/newsapp/NewsActivity.kt
+++ app/src/main/java/com/example/newsapp/NewsActivity.kt
@@ -2,10 +2,14 @@ package com.example.newsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.newsapp.databinding.ActivityNewsBinding
 
 class NewsActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityNewsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_news)
+
+        binding = ActivityNewsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
